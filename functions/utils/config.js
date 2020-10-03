7f905fb25fdd6347e0c496034f2d7616de6fbf3c
Dominique <Sevendayes@gmail.com>
2020-10-03 19:08:09 +0200

    avancement du projet

---
+++ functions/utils/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+exports.firebaseConfig = {
+    apiKey: "AIzaSyC3LjNqjpxtO4_m-HNzvrmDqODJAXf-M_I",
+    authDomain: "socialapp-60386.firebaseapp.com",
+    databaseURL: "https://socialapp-60386.firebaseio.com",
+    projectId: "socialapp-60386",
+    storageBucket: "socialapp-60386.appspot.com",
+    messagingSenderId: "572622214410",
+    appId: "1:572622214410:web:9585a61e948d22076f96f3",
+    measurementId: "G-J6M8V9KF00"
+};
